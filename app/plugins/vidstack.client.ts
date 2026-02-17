5af1ffa4040404cdbb318f7456a67d9047c6afa9
Athenox Development <athenox2008@gmail.com>
2026-02-17 02:22:21 +0100

    Replaced VideoPlayer

---
+++ app/plugins/vidstack.client.ts
@@ -0,0 +1,7 @@
+import 'vidstack/player/styles/default/theme.css'
+import 'vidstack/player/styles/default/layouts/video.css'
+import 'vidstack/player'
+import 'vidstack/player/layouts/default'
+import 'vidstack/player/ui'
+
+export default defineNuxtPlugin(() => {})
